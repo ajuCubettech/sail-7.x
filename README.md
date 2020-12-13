17de2f69a8774105155b132024f493af8a7685dc
aju <ajuchacko91@gmail.com>
2020-12-13 15:46:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Laravel Sail-7.x
+
+## Introduction
+
+Sail provides a Docker powered local development experience for Laravel that is compatible with macOS, Windows (WSL2), and Linux. Other than Docker, no software or libraries are required to be installed on your local computer before using Sail. Sail's simple CLI means you can start building your Laravel application without any previous Docker experience.
+
+## Official Documentation
+
+Documentation for Sail can be found on the [Laravel website](https://laravel.com/docs/sail).
+
+## License
+
+Laravel Sail is open-sourced software licensed under the [MIT license](LICENSE.md).
---
+++ src/SailServiceProvider.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace Ajuchacko\Sail;
+
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\ServiceProvider;
+
+class SailServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        if ($this->app->runningInConsole()) {
+            $this->registerCommands();
+            $this->configurePublishing();
+        }
+    }
+
+    /**
+     * Register the console commands for the package.
+     *
+     * @return void
+     */
+    protected function registerCommands()
+    {
+        Artisan::command('sail:install', function () {
+            copy(__DIR__.'/../stubs/docker-compose.yml', base_path('docker-compose.yml'));
+
+            $environment = file_get_contents(base_path('.env'));
+
+            $environment = str_replace('DB_HOST=127.0.0.1', 'DB_HOST=mysql', $environment);
+            $environment = str_replace('MEMCACHED_HOST=127.0.0.1', 'MEMCACHED_HOST=redis', $environment);
+            $environment = str_replace('REDIS_HOST=127.0.0.1', 'REDIS_HOST=redis', $environment);
+
+            file_put_contents(base_path('.env'), $environment);
+        });
+
+        Artisan::command('sail:publish', function () {
+            $this->call('vendor:publish', ['--tag' => 'sail']);
+
+            file_put_contents(base_path('docker-compose.yml'), str_replace(
+                './vendor/laravel/sail/runtimes/8.0',
+                './docker/8.0',
+                file_get_contents(base_path('docker-compose.yml'))
+            ));
+        });
+    }
+
+    /**
+     * Configure publishing for the package.
+     *
+     * @return void
+     */
+    protected function configurePublishing()
+    {
+        $this->publishes([
+            __DIR__.'/../runtimes' => base_path('docker'),
+        ], 'sail');
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'sail.install-command',
+            'sail.publish-command',
+        ];
+    }
+}
